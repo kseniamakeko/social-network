a3feed86ceb499b9043737358c3efd001b18e1a1
Ksenia <kseniamakeko@gmail.com>
2024-08-15 14:06:55 +0500

    refactoring dialogs, styles

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,8 +1,35 @@
 import React from "react";
-// import classes from "./Dialog.module.css";
+import { NavLink } from "react-router-dom";
+import classes from "./Dialogs.module.css";
 
-const Daialogs = (props) => {
-  return <div>Dialog</div>;
+const DialogItem = (props) => {
+  let path = `/dialogs/${props.id}`;
+  return (
+    <div className={classes.dialog + " " + classes.active}>
+      <NavLink to={path}>{props.name}</NavLink>
+    </div>
+  );
+};
+const Message = (props) => {
+  return <div className={classes.dialog}>{props.message}</div>;
 };
 
-export default Daialogs;
+const Dialogs = (props) => {
+  return (
+    <div className={classes.dialogs}>
+      <div className={classes["dialogs-items"]}>
+        <DialogItem name="Mila" id="1" />
+        <DialogItem name="Eugine" id="2" />
+        <DialogItem name="Scott" id="3" />
+        <DialogItem name="Graham" id="4" />
+      </div>
+      <div className={classes.messages}>
+        <Message message="Hi" />
+        <Message message="What's new?" />
+        <Message message="What's about meeting?" />
+      </div>
+    </div>
+  );
+};
+
+export default Dialogs;
